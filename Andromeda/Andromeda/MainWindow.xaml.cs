9146c77335f637c3897f7e9b77967a9436d228b1
SpeedSterKawaii <72403623+SpeedSterKawaii@users.noreply.github.com>
2021-05-19 10:06:39 -0400

    Add files via upload

---
+++ Andromeda/Andromeda/MainWindow.xaml.cs
@@ -0,0 +1,201 @@
+﻿using ICSharpCode.AvalonEdit.Highlighting;
+using ICSharpCode.AvalonEdit.Highlighting.Xshd;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Xml;
+
+namespace Andromeda
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            LuaEditor.Options.EnableHyperlinks = false;
+            LuaEditor.Options.EnableEmailHyperlinks = false;
+            LuaEditor.Options.AllowScrollBelowDocument = false;
+
+            using (var streamReader = new StreamReader("lua.xshd"))
+            {
+                using (var reader = new XmlTextReader(streamReader))
+                {
+                    LuaEditor.SyntaxHighlighting = ICSharpCode.AvalonEdit.Highlighting.Xshd.HighlightingLoader.Load(reader, HighlightingManager.Instance);
+                }
+            }
+        }
+
+        private void Top_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed)
+            {
+                this.DragMove();
+            }
+        }
+
+
+        private void ExitBtn_Click(object sender, MouseButtonEventArgs e)
+        {
+            Environment.Exit(0);
+
+        }
+
+        private void MinimizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Minimized;
+        }
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void MaximizeBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.WindowState = WindowState.Maximized;
+        }
+
+        private void AttachBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ea.Attach();
+
+        }
+        private void SrgsBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Settings lol = new Settings();
+            lol.Show();
+        }
+
+        private void ExecuteBtn_Click(object sender, RoutedEventArgs e)
+        {
+            ea.Execute(LuaEditor.Text, LuaEditor.Text, LuaEditor.Text);
+        }
+
+        andromeda ea = new andromeda();
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog opendialogfile = new OpenFileDialog();
+            opendialogfile.Filter = "Lua File (*.lua)|*.lua|Text File (*.txt)|*.txt";
+            opendialogfile.FilterIndex = 2;
+            opendialogfile.RestoreDirectory = true;
+            if (opendialogfile.ShowDialog() != DialogResult)
+                return;
+            try
+            {
+                LuaEditor.Text = "";
+                System.IO.Stream stream;
+                if ((stream = opendialogfile.OpenFile()) == null)
+                    return;
+                using (stream)
+                    this.LuaEditor.Text = System.IO.File.ReadAllText(opendialogfile.FileName);
+            }
+            catch (Exception)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        private void SaveBtn_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Txt Files (*.txt)|*.txt|Lua Files (*.lua)|*.lua|All Files (*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult)
+            {
+                Stream s = sfd.OpenFile();
+                StreamWriter sw = new StreamWriter(s);
+                sw.Write(LuaEditor.Text);
+                sw.Close();
+            }
+        }
+
+        private void ClearBtn_Click(object sender, RoutedEventArgs e)
+        {
+            LuaEditor.Text = "";
+        }
+
+        private void CloseRBLXBtn_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.ProcessName == "RobloxPlayerBeta")
+                {
+                    process.Kill();
+                }
+            }
+        }
+
+        private void LuaEditor_TextChanged(object sender, EventArgs e)
+        {
+            int count = LuaEditor.Text.Length;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Andromeda/Andromeda/andromeda.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Pipes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using Andromeda;
+using System.Windows.Forms;
+namespace Andromeda
+{
+    class andromeda
+    {
+        public void Execute(string lua, string pipe, string input) 
+        {
+                using (NamedPipeClientStream namedPipeClientStream = new NamedPipeClientStream(".", pipe, PipeDirection.Out))
+                {
+                    namedPipeClientStream.Connect();
+                    using (StreamWriter streamWriter = new StreamWriter(namedPipeClientStream))
+                    {
+                        streamWriter.Write(input);
+                        streamWriter.Dispose();
+                    }
+                    namedPipeClientStream.Dispose();
+                }
+        }
+
+        public void Attach() 
+        {
+            if (NamedPipeExist(luapipename))
+            {
+                MessageBox.Show("Andromeda is Already Attached into Roblox.", "Andromeda is Already Attached.", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                try
+                {
+                    Injector.InjectDLL();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Andromeda Injection Error: " + ex.Message, "Andromeda Injection Error.", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        public static string luapipename = "krnlpipe";
+        public static string dllname = "andromeda.dll";
+        BasicInject Injector = new BasicInject();
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool WaitNamedPipe(string name, int timeout);
+
+        public static bool NamedPipeExist(string pipeName)
+        {
+            bool result;
+            try
+            {
+                int timeout = 0;
+                if (!WaitNamedPipe(Path.GetFullPath(string.Format("\\\\.\\pipe\\{0}", pipeName)), timeout))
+                {
+                    int lastWin32Error = Marshal.GetLastWin32Error();
+                    if (lastWin32Error == 0)
+                    {
+                        return false;
+                    }
+                    if (lastWin32Error == 2)
+                    {
+                        return false;
+                    }
+                }
+                result = true;
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+            return result;
+        }
+
+        public class BasicInject
+        {
+            [DllImport("kernel32", CharSet = CharSet.Ansi, SetLastError = true)]
+            internal static extern IntPtr LoadLibraryA(string lpFileName);
+
+            [DllImport("kernel32", CharSet = CharSet.Ansi, ExactSpelling = true, SetLastError = true)]
+            internal static extern UIntPtr GetProcAddress(IntPtr hModule, string procName);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            [return: MarshalAs(UnmanagedType.Bool)]
+            internal static extern bool FreeLibrary(IntPtr hModule);
+
+            [DllImport("kernel32.dll")]
+            internal static extern IntPtr OpenProcess(BasicInject.ProcessAccess dwDesiredAccess, [MarshalAs(UnmanagedType.Bool)] bool bInheritHandle, int dwProcessId);
+
+            [DllImport("kernel32.dll", ExactSpelling = true, SetLastError = true)]
+            internal static extern IntPtr VirtualAllocEx(IntPtr hProcess, IntPtr lpAddress, uint dwSize, uint flAllocationType, uint flProtect);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            internal static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, uint nSize, out UIntPtr lpNumberOfBytesWritten);
+
+            [DllImport("kernel32.dll")]
+            internal static extern IntPtr CreateRemoteThread(IntPtr hProcess, IntPtr lpThreadAttributes, uint dwStackSize, UIntPtr lpStartAddress, IntPtr lpParameter, uint dwCreationFlags, out IntPtr lpThreadId);
+
+            [DllImport("kernel32.dll", SetLastError = true)]
+            internal static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, [Out] byte[] lpBuffer, int dwSize, out int lpNumberOfBytesRead);
+
+            public bool InjectDLL()
+            {
+                if (Process.GetProcessesByName("RobloxPlayerBeta").Length == 0)
+                {
+                    return false;
+                }
+                Process process = Process.GetProcessesByName("RobloxPlayerBeta")[0];
+                byte[] bytes = new ASCIIEncoding().GetBytes(AppDomain.CurrentDomain.BaseDirectory + dllname);
+                IntPtr hModule = BasicInject.LoadLibraryA("kernel32.dll");
+                UIntPtr procAddress = BasicInject.GetProcAddress(hModule, "LoadLibraryA");
+                BasicInject.FreeLibrary(hModule);
+                if (procAddress == UIntPtr.Zero)
+                {
+                    return false;
+                }
+                IntPtr intPtr = BasicInject.OpenProcess(BasicInject.ProcessAccess.AllAccess, false, process.Id);
+                if (intPtr == IntPtr.Zero)
+                {
+                    return false;
+                }
+                IntPtr intPtr2 = BasicInject.VirtualAllocEx(intPtr, (IntPtr)0, (uint)bytes.Length, 12288u, 4u);
+                UIntPtr uintPtr;
+                IntPtr intPtr3;
+                return !(intPtr2 == IntPtr.Zero) && BasicInject.WriteProcessMemory(intPtr, intPtr2, bytes, (uint)bytes.Length, out uintPtr) && !(BasicInject.CreateRemoteThread(intPtr, (IntPtr)0, 0u, procAddress, intPtr2, 0u, out intPtr3) == IntPtr.Zero);
+            }
+
+            [Flags]
+            public enum ProcessAccess
+            {
+                AllAccess = 1050235,
+                CreateThread = 2,
+                DuplicateHandle = 64,
+                QueryInformation = 1024,
+                SetInformation = 512,
+                Terminate = 1,
+                VMOperation = 8,
+                VMRead = 16,
+                VMWrite = 32,
+                Synchronize = 1048576
+            }
+        }
+    }
+}
